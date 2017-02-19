921f108911d45084aa025fc765d948e8fe828123
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2017-02-19 14:57:02 +0100

    calculate max height

--- src/pages/object/object.ts
+++ src/pages/object/object.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-object',
   templateUrl: 'object.html'
 })
-export class ObjectPage {
+export class ObjectPage implements OnInit {
+  private maxHeight: number;
 
   constructor(public navCtrl: NavController) {
-
+    window.setTimeout(this.tick, 10000);
+    this.maxHeight = 4711;
   }
 
+  ngOnInit() {
+     this.maxHeight = window.innerHeight;
+  }
+
+  tick() {
+    alert(this.maxHeight);
+  }
 }
